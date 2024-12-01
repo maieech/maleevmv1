d97cbf2494df74e735b25f9c4e1a110ed44a65d3
maieech <maleevmv@mer.ci.nsu.ru>
2024-12-01 20:22:22 +0700

    Add files via upload

---
+++ Chain of Responsibility Pattern/Program.cs
@@ -0,0 +1,116 @@
+﻿public abstract class RequestHandler
+{
+    protected RequestHandler _nextHandler;
+
+    public void SetNextHandler(RequestHandler nextHandler)
+    {
+        _nextHandler = nextHandler;
+    }
+
+    public abstract void HandleRequest(Request request);
+}
+
+
+
+public class ErrorHandler : RequestHandler
+{
+    public override void HandleRequest(Request request)
+    {
+        if (request.Type == RequestType.Error)
+        {
+            Console.WriteLine("Обрабатываю ошибку: " + request.Message);
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+
+
+public class LoggingHandler : RequestHandler
+{
+    public override void HandleRequest(Request request)
+    {
+        if (request.Type == RequestType.Log)
+        {
+            Console.WriteLine("Логирование события: " + request.Message);
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+
+
+public class NotificationHandler : RequestHandler
+{
+    public override void HandleRequest(Request request)
+    {
+        if (request.Type == RequestType.Notification)
+        {
+            Console.WriteLine("Отправка уведомления: " + request.Message);
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+
+
+public class Request
+{
+    public RequestType Type { get; set; }
+    public string Message { get; set; }
+
+    public Request(RequestType type, string message)
+    {
+        Type = type;
+        Message = message;
+    }
+}
+
+
+
+public enum RequestType
+{
+    Error,
+    Log,
+    Notification
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {       
+        var errorHandler = new ErrorHandler();
+        var loggingHandler = new LoggingHandler();
+        var notificationHandler = new NotificationHandler();
+
+  
+        errorHandler.SetNextHandler(loggingHandler);
+        loggingHandler.SetNextHandler(notificationHandler);
+
+ 
+        var errorRequest = new Request(RequestType.Error, "Ошибка сервера");
+        var logRequest = new Request(RequestType.Log, "Логирование события");
+        var notificationRequest = new Request(RequestType.Notification, "Новый комментарий");
+
+
+        Console.WriteLine("Обработка запроса о ошибке:");
+        errorHandler.HandleRequest(errorRequest);
+
+        Console.WriteLine("\nОбработка запроса о логировании:");
+        errorHandler.HandleRequest(logRequest);
+
+        Console.WriteLine("\nОбработка запроса о уведомлении:");
+        errorHandler.HandleRequest(notificationRequest);
+    }
+}
---
+++ Command Pattern/Program.cs
@@ -0,0 +1,112 @@
+﻿public interface ICommand
+{
+    void Execute();  // Выполнить команду
+    void Undo();     // Отменить команду
+}
+public class InsertTextCommand : ICommand
+{
+    private readonly TextEditor _editor;
+    private readonly string _text;
+
+    public InsertTextCommand(TextEditor editor, string text)
+    {
+        _editor = editor;
+        _text = text;
+    }
+
+    public void Execute()
+    {
+        _editor.InsertText(_text);
+    }
+
+    public void Undo()
+    {
+        _editor.DeleteText(_text.Length);
+    }
+}
+public class DeleteTextCommand : ICommand
+{
+    private readonly TextEditor _editor;
+    private readonly int _length;
+    private string _deletedText;
+
+    public DeleteTextCommand(TextEditor editor, int length)
+    {
+        _editor = editor;
+        _length = length;
+    }
+
+    public void Execute()
+    {
+        _deletedText = _editor.DeleteText(_length);
+    }
+
+    public void Undo()
+    {
+        _editor.InsertText(_deletedText);
+    }
+}
+
+
+public class TextEditor
+{
+    private string _text = "";
+    private Stack<ICommand> _history = new Stack<ICommand>();
+
+
+    public void InsertText(string text)
+    {
+        _text += text;
+        Console.WriteLine($"Текущий текст: {_text}");
+    }
+
+    public string DeleteText(int length)
+    {
+        if (length > _text.Length)
+        {
+            length = _text.Length;
+        }
+
+        string deletedText = _text.Substring(_text.Length - length);
+        _text = _text.Substring(0, _text.Length - length);
+        Console.WriteLine($"Текущий текст: {_text}");
+        return deletedText;
+    }
+
+    public void ExecuteCommand(ICommand command)
+    {
+        command.Execute();
+        _history.Push(command);
+    }
+
+    public void UndoLastCommand()
+    {
+        if (_history.Count > 0)
+        {
+            ICommand lastCommand = _history.Pop();
+            lastCommand.Undo();
+        }
+        else
+        {
+            Console.WriteLine("Нет команд для отмены.");
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        TextEditor editor = new TextEditor();
+
+        // Тестирование команд
+        ICommand insertHello = new InsertTextCommand(editor, "Hello ");
+        ICommand insertWorld = new InsertTextCommand(editor, "World!");
+        ICommand deleteWorld = new DeleteTextCommand(editor, 6);  // Удалить "World!"
+
+        editor.ExecuteCommand(insertHello);
+        editor.ExecuteCommand(insertWorld);
+        editor.UndoLastCommand();
+        editor.UndoLastCommand();
+        editor.UndoLastCommand();
+    }
+}
