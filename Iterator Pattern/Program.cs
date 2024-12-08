dc760471ce787c0a5034215af2032eb631e2bf79
maieech <maleevmv@mer.ci.nsu.ru>
2024-12-08 22:26:44 +0700

    Add files via upload

---
+++ Iterator Pattern/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+
+
+    public override string ToString()
+    {
+        return $"Title: {Title}, Author: {Author}, Year: {Year}";
+    }
+}
+
+
+public class Library : IEnumerable<Book>
+{
+    private List<Book> books = new List<Book>();
+
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        foreach (var book in books)
+        {
+            yield return book;
+        }
+    }
+
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var book1 = new Book("Война и мир", "Лев Толстой", 1869);
+        var book2 = new Book("Преступление и наказание", "Фёдор Достоевский", 1866);
+        var book3 = new Book("Евгений Онегин", "Александр Пушкин", 1833);
+
+        var library = new Library();
+        library.AddBook(book1);
+        library.AddBook(book2);
+        library.AddBook(book3);
+
+        foreach (var book in library)
+        {
+            Console.WriteLine(book);
+        }
+    }
+}
---
+++ Strategy Pattern/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IWeapon
+{
+    void UseWeapon();
+}
+
+public class Sword : IWeapon
+{
+    public void UseWeapon()
+    {
+        Console.WriteLine("Игрок взмахивает мечом: наносит сильный удар!");
+    }
+}
+
+public class Bow : IWeapon
+{
+    public void UseWeapon()
+    {
+        Console.WriteLine("Игрок использует лук: выпускает стрелу!");
+    }
+}
+
+public class Axe : IWeapon
+{
+    public void UseWeapon()
+    {
+        Console.WriteLine("Игрок использует топор: наносит мощный рубящий удар!");
+    }
+}
+
+public class Player
+{
+    private IWeapon _weapon;
+
+
+    public Player(IWeapon weapon)
+    {
+        _weapon = weapon;
+    }
+
+
+    public void SetWeapon(IWeapon weapon)
+    {
+        _weapon = weapon;
+    }
+
+    public void Attack()
+    {
+        _weapon.UseWeapon();
+    }
+}
+
+public class Game
+{
+    private Player _player;
+
+    public Game(Player player)
+    {
+        _player = player;
+    }
+
+    public void ChooseWeapon()
+    {
+        Console.WriteLine("Выберите оружие:");
+        Console.WriteLine("1 - Меч");
+        Console.WriteLine("2 - Лук");
+        Console.WriteLine("3 - Топор");
+        Console.Write("Введите номер: ");
+
+        string choice = Console.ReadLine();
+        IWeapon selectedWeapon = null;
+
+        switch (choice)
+        {
+            case "1":
+                selectedWeapon = new Sword();
+                break;
+            case "2":
+                selectedWeapon = new Bow();
+                break;
+            case "3":
+                selectedWeapon = new Axe();
+                break;
+            default:
+                Console.WriteLine("Неверный выбор.");
+                return;
+        }
+
+        _player.SetWeapon(selectedWeapon);
+        Console.WriteLine("Оружие выбрано!");
+    }
+
+    public void AttackWithWeapon()
+    {
+        _player.Attack();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var player = new Player(new Sword());
+        var game = new Game(player);
+
+        while (true)
+        {
+            Console.WriteLine("\n-- Игровое меню --");
+            Console.WriteLine("1 - Выбрать оружие");
+            Console.WriteLine("2 - Атаковать");
+            Console.WriteLine("3 - Выйти");
+            Console.Write("Ваш выбор: ");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                game.ChooseWeapon();
+            }
+            else if (input == "2")
+            {
+                game.AttackWithWeapon();
+            }
+            else if (input == "3")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор, попробуйте снова.");
+            }
+        }
+    }
+}
+
